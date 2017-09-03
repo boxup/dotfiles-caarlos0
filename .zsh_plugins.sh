c1b0eae677f5f0be00be02d71088ddc7bf12b2d3
BaseBoxOrg <baseboxorg@outlook.com>
2017-09-03 17:48:23 +0800

    update new

---
+++ .zsh_plugins.sh
@@ -0,0 +1,16 @@
+export PATH="/Volumes/M-Data/Users/VooBoy/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-git-add-remote:$PATH"
+source /Volumes/M-Data/Users/VooBoy/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-jvm/jvm.plugin.zsh
+export PATH="/Volumes/M-Data/Users/VooBoy/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-ports:$PATH"
+export PATH="/Volumes/M-Data/Users/VooBoy/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-zsh-git-fetch-merge:$PATH"
+export PATH="/Volumes/M-Data/Users/VooBoy/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-zsh-git-sync:$PATH"
+source /Volumes/M-Data/Users/VooBoy/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-zsh-mkc/mkc.plugin.zsh
+export PATH="/Volumes/M-Data/Users/VooBoy/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-zsh-open-pr:$PATH"
+source /Volumes/M-Data/Users/VooBoy/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
+source /Volumes/M-Data/Users/VooBoy/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z/z.sh
+source /Volumes/M-Data/Users/VooBoy/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Tarrasch-SLASH-zsh-bd/bd.plugin.zsh
+source /Volumes/M-Data/Users/VooBoy/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-wbinglee-SLASH-zsh-wakatime/zsh-wakatime.plugin.zsh
+source /Volumes/M-Data/Users/VooBoy/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+source /Volumes/M-Data/Users/VooBoy/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+source /Volumes/M-Data/Users/VooBoy/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
+source /Volumes/M-Data/Users/VooBoy/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+source /Volumes/M-Data/Users/VooBoy/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
